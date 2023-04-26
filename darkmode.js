4d529b9bea7359fcbdb489ef2d8b4f19c4bd4861
Gabriel <gabmt2210@gmail.com>
2023-04-26 10:56:45 -0300

    estilo v1

---
+++ darkmode.js
@@ -0,0 +1,6 @@
+function trocar(cor) {
+    const header = document.querySelector('.header-page');
+    const bgImageBlank = document.querySelector('.bg-image-blank');
+    header.style.backgroundColor = cor;
+    bgImageBlank.style.backgroundColor = cor;
+  }
